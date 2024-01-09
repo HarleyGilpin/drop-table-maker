b15a9bb9a27e80382cb79ca0748e631a47068299
Harley <hg@harleygilpin.com>
2024-01-09 04:04:10 -0800

    feat: add running total of slots in the generated drop table script

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import IntVar
+from tkinter import messagebox
 from ttkthemes import ThemedTk
+import json
+
+
 
 class Application(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.master.title("2011Scape - Drop Table Maker by Pixel [Version 1.0]")
+        self.master.title("2011Scape - Drop Table Maker by Pixel [Version 1.2]")
+        self.create_menu()  # This will create our menu
 
         # Get the path of the directory containing the executable or the script
         if getattr(sys, 'frozen', False):
@@ -53,6 +58,67 @@ class Application(ttk.Frame):
         for _ in range(1):  # Add items by default
             self.add_item_row()
 
+    def create_menu(self):
+        menubar = tk.Menu(self.master)
+        
+        # Create a File menu
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Save", command=self.export_changes)
+        filemenu.add_command(label="Load", command=self.import_changes)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.master.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+        
+        self.master.config(menu=menubar)
+
+    def export_changes(self):
+        file = filedialog.asksaveasfilename(defaultextension=".json",
+                                            filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
+        if not file:
+            return
+
+        data = {
+            "total_slots": self.total_slots.get(),
+            "items": []
+        }
+
+        for item in self.items:
+            item_data = {
+                "name": item[0].get(),
+                "quantity": item[1].get(),
+                "probability": item[2].get(),
+                "guaranteed": item[3].get()
+            }
+            data["items"].append(item_data)
+
+        with open(file, "w") as f:
+            json.dump(data, f)
+
+    def import_changes(self):
+        file = filedialog.askopenfilename(defaultextension=".json",
+                                        filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
+        if not file:
+            return
+
+        with open(file, "r") as f:
+            data = json.load(f)
+
+        self.total_slots.set(data["total_slots"])
+
+        # Clear existing items
+        for item in self.items:
+            self.remove_item(item)
+
+        # Load items from the save
+        for item_data in data["items"]:
+            self.add_item_row()
+            current_item = self.items[-1]  # The last added item
+            current_item[0].set(item_data["name"])
+            current_item[1].delete(0, tk.END)
+            current_item[1].insert(0, item_data["quantity"])
+            current_item[2].set(item_data["probability"])
+            current_item[3].set(item_data["guaranteed"])
+        
     def create_widgets(self):
             ttk.Label(self, text="Total slots").grid(row=0, column=0)
 
@@ -131,11 +197,11 @@ class Application(ttk.Frame):
         return int(total * probability)
 
     def generate_code(self):
-        total_slots = self.total_slots.get()
+        total_slots = int(self.total_slots.get())
+        running_total = 0  # Initialize running total
         guaranteed_code = f'    guaranteed {{\n'
         main_code = f'    main {{\n'
         main_code += f'        total({total_slots})\n'  # total slots function
-
 
         for item in self.items:
             name = item[0].get() if item[0].get() != "" else "BONES"
@@ -155,8 +221,15 @@ class Application(ttk.Frame):
             else:
                 probability = item[2].get()
                 slots = self.calculate_slots(probability)
+                running_total += slots  # Add slots to the running total
                 main_code += f'        obj(Items.{name.upper()}, {quantity_param}, slots = {slots})\n'
-        
+
+        # Check if running_total surpasses total_slots and display a warning
+        over_by = running_total - total_slots
+        if over_by > 0:
+            messagebox.showwarning("Warning", f"The running total of slots is greater than the total slots selected by {over_by}!")
+            return
+
         guaranteed_code += '    }\n'
         main_code += '    }\n'
 
@@ -164,6 +237,7 @@ class Application(ttk.Frame):
         code += 'val ids = intArrayOf(Npcs.YOUR_NPC_ID)\n\n'
         code += 'val table = DropTableFactory.build {\n'
         code += guaranteed_code
+        code += f'\t/** Running total: {running_total} **/\n'
         code += main_code
         code += '}\n\n'
         self.save_code(code)
